f3a7f805ebe4194441e63848744b136956925ad0
Rafael Luiz de Macedo <rafael.macedo-montreal@pbh.gov.br>
2026-02-22 12:09:02 -0300

    Criado rota PUT para atualizar o titulo e data, criado validação na request e padronizado fazendo uso na store e update.

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -5,6 +5,8 @@ class ListItemsController < ApplicationController
     end
 
     def store
+        return required_fields unless validate_request
+
         item = ListItem.new(item_params)
 
         if item.save
@@ -15,12 +17,44 @@ class ListItemsController < ApplicationController
         end
     end
 
+    def update
+        return required_fields unless validate_request
+
+        list_item = ListItem.findTitleWithDate(params)
+
+        if list_item
+            update_params = params.permit(:titulo_novo, :data_novo).to_h.compact
+
+            item_updated = {}
+            item_updated[:titulo] = update_params[:titulo_novo] if update_params.key?(:titulo_novo)
+            item_updated[:data]   = update_params[:data_novo] if update_params.key?(:data_novo)
+
+            if item_updated.any?
+            list_item.update(item_updated)
+            end
+            
+            render json: list_item, status: :ok
+        else
+            not_found
+        end
+    end
+
     private
 
     def item_params
         params.permit(:titulo, :data)
     end
 
+    def item_params_update
+        params.permit(:titulo, :titulo_novo, :data, :data_novo)
+    end
+
+    def validate_request
+        unless params.permit(:titulo).present? && params.permit(:data).present?
+            return false
+        end
+        return true
+    end
     def store_dependencies(item)
         return unless params[:dependencias].present?
 
--- spec/requests/list_items_spec.rb
+++ spec/requests/list_items_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe "Controler List Items API", type: :request do
     it "Return all items with your dependencies" do
       ListItem.create!(titulo: "Item 1", data: Time.now)
       ListItem.create!(titulo: "Item 2", data: Time.now)
-      ListItem.create!(titulo: "Item Dependencia 3", data: Time.now)
+      ListItem.create!(titulo: "Item 3", data: Time.now)
   
       get "/item"
   
@@ -70,22 +70,22 @@ RSpec.describe "Controler List Items API", type: :request do
       expect(JSON.parse(response.body).size).to eq(3)
     end
   end
-  
+
   describe "GET /item?titulo=Tarefa A" do
     it "Search the title" do
-      title = "Tarefa A"
+      item_title = "Tarefa A"
       title_expected = "Tarefa A"
 
-      ListItem.create!(titulo: title, data: Time.now)
+      ListItem.create!(titulo: item_title, data: Time.now)
       ListItem.create!(titulo: "Item 2", data: Time.now)
   
-      get "/item?titulo=" + title
+      get "/item?titulo=" + item_title
   
       expect(response).to have_http_status(:ok)
 
-      responseJson = JSON.parse(response.body)
+      response_json = JSON.parse(response.body)
 
-      expect(responseJson.first["titulo"]).to eq(title_expected)
+      expect(response_json.first["titulo"]).to eq(title_expected)
     end
   end
   
@@ -104,10 +104,10 @@ RSpec.describe "Controler List Items API", type: :request do
   
       expect(response).to have_http_status(:ok)
 
-      responseJson = JSON.parse(response.body)
+      response_json = JSON.parse(response.body)
 
-      expect(responseJson.first["titulo"]).to eq(title_expected)
-      expect(responseJson.first["data"]).to eq(date_expected)
+      expect(response_json.first["titulo"]).to eq(title_expected)
+      expect(response_json.first["data"]).to eq(date_expected)
     end
   end
   
@@ -128,12 +128,138 @@ RSpec.describe "Controler List Items API", type: :request do
   
       expect(response).to have_http_status(:ok)
 
-      responseJson = JSON.parse(response.body)
+      response_json = JSON.parse(response.body)
 
-      expect(responseJson.first["titulo"]).to eq(title_expected)
-      expect(responseJson.first["data"]).to eq(date_expected)
-      expect(responseJson.first["data"]).not_to eq(item_date2.strftime("%d/%m/%Y"))
+      expect(response_json.first["titulo"]).to eq(title_expected)
+      expect(response_json.first["data"]).to eq(date_expected)
+      expect(response_json.first["data"]).not_to eq(item_date2.strftime("%d/%m/%Y"))
     end
   end
+
+  describe "PUT /item" do
+    it "Not change item because item not found" do
+      item_title = "Tarefa A"
+      item_date = Time.now
+      title_expected = "Tarefa A"
+
+      ListItem.create!(titulo: item_title, data: item_date)
+      ListItem.create!(titulo: "Tarefa B", data: Time.now)
+  
+      get "/item?titulo=" + item_title
+  
+      expect(response).to have_http_status(:ok)
+      response_json = JSON.parse(response.body)
+      expect(response_json.first["titulo"]).to eq(title_expected)
+
+      item_title_not_exist = "Tarefa Not Exist"
+      item_title_change = "Tarefa Alterado Titulo"
+      
+      put "/item",
+           params: {
+             titulo: item_title_not_exist,
+             data: item_date,
+             titulo_novo: item_title_change
+           },
+           as: :json
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "PUT /item" do
+    it "Not change item when not send title in request" do
+      item_title = "Tarefa A"
+      item_date = Time.now
+      title_expected = "Tarefa A"
+
+      ListItem.create!(titulo: item_title, data: item_date)
+      ListItem.create!(titulo: "Tarefa B", data: Time.now)
+  
+      get "/item?titulo=" + item_title
+  
+      expect(response).to have_http_status(:ok)
+      response_json = JSON.parse(response.body)
+      expect(response_json.first["titulo"]).to eq(title_expected)
+
+      item_title_not_exist = "Tarefa Not Exist"
+      item_title_change = "Tarefa Alterado Titulo"
+      
+      put "/item",
+           params: {
+             data: item_date,
+             titulo_novo: item_title_change
+           },
+           as: :json
+
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  describe "PUT /item" do
+    it "Not change item when not send date in request" do
+      item_title = "Tarefa A"
+      item_date = Time.now
+      title_expected = "Tarefa A"
+
+      ListItem.create!(titulo: item_title, data: item_date)
+      ListItem.create!(titulo: "Tarefa B", data: Time.now)
+  
+      get "/item?titulo=" + item_title
+  
+      expect(response).to have_http_status(:ok)
+      response_json = JSON.parse(response.body)
+      expect(response_json.first["titulo"]).to eq(title_expected)
+
+      item_title_not_exist = "Tarefa Not Exist"
+      item_title_change = "Tarefa Alterado Titulo"
+      
+      put "/item",
+           params: {
+            titulo: item_title,
+             titulo_novo: item_title_change
+           },
+           as: :json
+
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  describe "PUT /item" do
+    it "Change the title" do
+      item_title = "Tarefa A"
+      item_date = Date.parse("2026-02-22").to_datetime
+      title_expected = "Tarefa A"
+
+      ListItem.create!(titulo: item_title, data: item_date)
+      ListItem.create!(titulo: "Tarefa B", data: Time.now)
+  
+      get "/item?titulo=" + item_title
+  
+      expect(response).to have_http_status(:ok)
+      response_json = JSON.parse(response.body)
+      expect(response_json.first["titulo"]).to eq(title_expected)
+
+      item_title_old = response_json.first["titulo"]
+      item_title_change = "Tarefa Alterado Titulo"
+      
+      put "/item",
+           params: {
+             titulo: item_title,
+             data: item_date,
+             titulo_novo: item_title_change
+           },
+           as: :json
+
+      expect(response).to have_http_status(:ok)
+
+      title_expected = "Tarefa Alterado Titulo"
+
+      response_json = JSON.parse(response.body)
+
+      expect(response_json["titulo"]).to eq(title_expected)
+      expect(response_json["titulo"]).not_to eq(item_title_old)
+    end
+  end
+  
 end
 
